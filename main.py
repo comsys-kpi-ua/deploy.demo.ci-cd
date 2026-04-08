0a01124c0e486041b7089a72975a0b0944dcc737
Oleksandr Kovalchuk <anxolerd@outlook.com>
2026-04-09 02:50:37 +0300

    Implement a simple web application

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from deploy-demo-ci-cd!")
+from flask import Flask
+import os
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    version = os.getenv("APP_VERSION", "1.0.0")
+    return f"<h1>Hello! App Version: {version}</h1>"
+
+@app.route('/health')
+def health():
+    if os.path.exists("crash.txt"):
+        return "Internal Server Error", 500
+    return "OK", 200
 
 if __name__ == "__main__":
-    main()
+    app.run(host='0.0.0.0', port=5000)
